9a354371de485801e978361a6039952c02ffdab7
mnjit20 <mnjit.vrma@gmail.com>
2018-06-27 01:13:51 +0530

    Promise chain

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 "# learn-javascript" 
+
+"# Callback : "
+
+"# Promise: Object that keep track about whether a certain event has happened already or not" 
+
+
--- promises.js
+++ promises.js
@@ -1,28 +1,82 @@
-console.log('********** Promises **********');
+/*Some old type async code.
+callback hell example*/
 
-//Some old type async code.
-function getRecipe() {
-  setTimeout(() => {
-    const recipeID = [521, 673, 886, 998, 1002];
-    console.log(recipeID);
+// function getRecipe() {
+//   console.log('********** callback hell functions invoked **********');
+//   setTimeout(() => {
+//     const recipeID = [521, 673, 886, 998, 1002];
+//     console.log(recipeID);
 
-    setTimeout((id) => {
-      const recipe = {
-        title: 'White sauce pasta',
-        publisher: 'Xyz Foods'
-      }
-      console.log(`${id}: ${recipe.title}`);
-      setTimeout((publisher) => {
-        const recipe2 = {
-          title: 'Red sauce pasta',
-          publisher: 'Italian foods'
-        }
-        console.log(recipe2);
-      }, 1500, recipe.publisher);
-    }, 1500, recipeID[2]);
+//     setTimeout((id) => {
+//       const recipe = {
+//         title: 'White sauce pasta',
+//         Restaurant: 'Xyz Foods'
+//       }
+//       console.log(`${id}: ${recipe.title}`);
+//       setTimeout((Restaurant) => {
+//         const recipe2 = {
+//           title: 'Red sauce pasta',
+//           Restaurant: 'Italian foods'
+//         }
+//         console.log(recipe2);
+//       }, 1500, recipe.Restaurant);
+//     }, 1500, recipeID[2]);
 
-  }, 1500);
-}
+//   }, 1500);
+// }
 
 //invoked
-getRecipe();
+//getRecipe();
+
+
+
+console.log('***************************');
+console.log('Callback Hell to Promises');
+//implmenting promises
+const getIds = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve([521, 673, 886, 998, 1002]);
+  }, 1500);
+});
+
+const getRecipe = recipeId => {
+  return new Promise((resolve, reject) => {
+    setTimeout((ID) => {
+      const recipe = {
+        title: 'Italian Pizza',
+        Restaurant: 'German Bakers',
+        id: ID
+      };
+      resolve(recipe);
+    }, 1500, recipeId);
+  });
+};
+
+const getRecommendation = RestaurantName => {
+  return new Promise((resolve, reject) => {
+    setTimeout((pubID) => {
+      const recRecipe = {
+        title: 'Maxican Pizza',
+        Restaurant: 'German Bakers',
+        id: pubID
+      }
+      resolve(recRecipe);
+    }, 1500, RestaurantName)
+  });
+};
+
+getIds
+  .then(IDs => {
+    console.log('Restaurant IDs List', IDs);
+    return getRecipe(IDs[2]);
+  })
+  .then(recipe => {
+    console.log('Recipe from Restaurant:', recipe);
+    return getRecommendation(recipe.id);
+  })
+  .then(recRecipe => {
+    console.log('Recommended from Restaurant :', recRecipe);
+  })
+  .catch(error => {
+    console.log('Error!! ', error);
+  });
