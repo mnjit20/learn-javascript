6a2ec8a0cdc5a98d81e7506cc014820a1fd95ef5
mnjit20 <mnjit.vrma@gmail.com>
2018-06-27 00:23:52 +0530

    old style async code

--- promises.js
+++ promises.js
@@ -1,1 +1,28 @@
 console.log('********** Promises **********');
+
+//Some old type async code.
+function getRecipe() {
+  setTimeout(() => {
+    const recipeID = [521, 673, 886, 998, 1002];
+    console.log(recipeID);
+
+    setTimeout((id) => {
+      const recipe = {
+        title: 'White sauce pasta',
+        publisher: 'Xyz Foods'
+      }
+      console.log(`${id}: ${recipe.title}`);
+      setTimeout((publisher) => {
+        const recipe2 = {
+          title: 'Red sauce pasta',
+          publisher: 'Italian foods'
+        }
+        console.log(recipe2);
+      }, 1500, recipe.publisher);
+    }, 1500, recipeID[2]);
+
+  }, 1500);
+}
+
+//invoked
+getRecipe();
