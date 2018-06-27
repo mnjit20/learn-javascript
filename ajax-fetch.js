57a674cf323da633a4e7cc077f8a6f251609befa
mnjit20 <mnjit.vrma@gmail.com>
2018-06-27 15:32:19 +0530

    Async await - loading news feeds

--- ajax-fetch.js
+++ ajax-fetch.js
@@ -42,11 +42,54 @@ const getRecommendation = RestaurantName => {
 //ajax calls using fetch
 //fetching using DainikBhaskar API
 
-fetch('https://appfeedlight.bhaskar.com/webfeed/news/521/521/2/0/15/').then(result => {
-  console.log(result);
-  return result.json();
-}).then(data => {
-  // console.log(data);
-  const firstStory = data.data;
-  console.log(firstStory);
-}).catch(error => console.log(error));
+
+const getNewsIDs = new Promise((resolve, reject) => {
+  fetch('https://appfeedlight.bhaskar.com/webfeed/news/521/521/2/0/15/').then(result => {
+    // console.log(result);
+    return result.json();
+  }).then(data => {
+    // console.log(data);
+    const firstStory = data.data.story;
+    stories = data.data.story;
+    // console.log(stories);
+    const story_list = [];
+    for (var i = 0; i < stories.length; i++) {
+      //    console.log(stories[i]['story_id']);
+      story_list.push(stories[i]['story_id']);
+    }
+    // console.log(story_list);
+    resolve(story_list);
+  }).catch(error => console.log(error));
+
+});
+const getNewsDetails = (IDs) => {
+
+  return new Promise((resolve, reject) => {
+
+    fetch(`https://appfeedlight.bhaskar.com/webfeed/articaldetail/521/${IDs}`).then(result => {
+      // console.log(result);
+      return result.json();
+    }).then(data => {
+      // console.log(data);
+      const firstStory = data.data.story;
+      stories = data.data.story;
+      // console.log(stories);
+      resolve(stories);
+    }).catch(error => console.log(error));
+  });
+
+};
+
+
+async function getStoryContent() {
+  const IDs = await getNewsIDs;
+  console.log(IDs);
+
+  const news = [];
+  for (var i = 0; i < IDs.length; i++) {
+    news[i] = await getNewsDetails(IDs[i]);
+    console.log('Story Id: ', news[i]['storyid'], ' - ', news[i]['title']);
+  }
+}
+
+getStoryContent();
