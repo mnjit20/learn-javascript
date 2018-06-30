641237b068f4f43e833d1f68ff96d38112dda465
mnjit20 <mnjit.vrma@gmail.com>
2018-06-30 16:45:11 +0530

    async await

--- async-github.js
+++ async-github.js
@@ -1,16 +1,36 @@
   const fetch = require('node-fetch');
 
-  async function showGitHubUser(handle) {
-    const url = `https://api.github.com/users/${handle}`;
+  // async function showGitHubUser(handle) {
+  //   const url = `https://api.github.com/users/${handle}`;
+  //   const response = await fetch(url);
+  //   const user = await response.json();
+  //   //console.log(user.name);
+  //   return user;
+  // }
+
+
+
+  // aa = showGitHubUser('mnjit20').then(user => {
+  //   console.log(user.name);
+  //   console.log(user.location);
+  // })
+
+  async function showGitHubUser(endpoint) {
+    const url = `https://api.github.com${endpoint}`;
     const response = await fetch(url);
-    const user = await response.json();
-    //console.log(user.name);
-    return user;
+    return await response.json();
+  }
+
+  async function showUserRepos(handle) {
+    const user = showGitHubUser(`/users/${handle}`);
+
+
+    //const repos = await showGitHubUser(`/users/${handle}/repos`);
+
+    console.log(user.name);
+    //console.log(repos.length, ' repos');
+
   }
 
 
-
-  aa = showGitHubUser('mnjit20').then(user => {
-    console.log(user.name);
-    console.log(user.location);
-  })
+  showUserRepos('mnjit20');
